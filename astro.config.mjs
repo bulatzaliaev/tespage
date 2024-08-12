5f54f0f599c9b49381d29f3d76100da85ab67976
Bulat <bulat8&@gmail.com>
2024-08-12 23:41:32 +0200

    fix site layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bulatzaliaev.github.io/',
-  base: '/',
   integrations: [react(), tailwind({
     applyBaseStyles: false,
   })]
